c204f6e94f0e3787e438a1545d6ad75a91a7d533
btomasini <btomasini@activated.io>
2021-09-08 09:07:22 -0700

    Created blocking test support

--- micronaut-cart/src/main/java/io/activated/pipeline/micronaut/cart/SetAddressBlockingReducer.java
+++ micronaut-cart/src/main/java/io/activated/pipeline/micronaut/cart/SetAddressBlockingReducer.java
@@ -2,18 +2,16 @@ package io.activated.pipeline.micronaut.cart;
 
 import io.activated.pipeline.BlockingReducer;
 import io.activated.pipeline.PipelineException;
-import io.activated.pipeline.Reducer;
 import io.activated.pipeline.annotations.Operation;
-import org.reactivestreams.Publisher;
 
 import javax.inject.Singleton;
 
 @Operation
 @Singleton
-public class SetAddressReducer implements Reducer<Cart, SetAddress> {
+public class SetAddressBlockingReducer implements BlockingReducer<Cart, SetAddress> {
 
   @Override
-  public Publisher<Cart> reduce(Cart state, SetAddress action) {
+  public void blockingReduce(Cart state, SetAddress action) {
 
     switch (action.getAddressType()) {
       case "B":
