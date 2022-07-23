7f5cae6bf425c2395e3d1d20fe1e6a9e70d3508f
Ben Tomasini <btomasini@activated.io>
2022-07-23 12:33:12 -0700

    validated that pipeline session id is a v4 guid

--- core/src/main/java/io/activated/pipeline/key/SessionKeyStrategy.java
+++ core/src/main/java/io/activated/pipeline/key/SessionKeyStrategy.java
@@ -2,10 +2,15 @@ package io.activated.pipeline.key;
 
 import io.activated.pipeline.Constants;
 import io.activated.pipeline.Context;
+import java.util.UUID;
 import org.reactivestreams.Publisher;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import reactor.core.publisher.Mono;
 
 public class SessionKeyStrategy implements KeyStrategy {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(SessionKeyStrategy.class);
 
   @Override
   public Publisher<Key> apply(Context context) {
@@ -16,7 +21,17 @@ public class SessionKeyStrategy implements KeyStrategy {
               if (sessionId == null) {
                 throw new IllegalStateException("pipeline-session-id not provided in header");
               }
-              return sessionId.get(0);
+              var raw = sessionId.get(0);
+              try {
+                var got = UUID.fromString(raw);
+                if (got.version() != 4) {
+                  throw new IllegalStateException("uuid is not v4");
+                }
+              } catch (Exception e) {
+                LOGGER.error("error reading pipeline-session-id", e);
+                throw new IllegalStateException("invalid pipeline-session-id");
+              }
+              return raw;
             })
         .map(
             id -> {
--- core/src/test/java/io/activated/pipeline/key/SessionKeyStrategyTest.java
+++ core/src/test/java/io/activated/pipeline/key/SessionKeyStrategyTest.java
@@ -6,15 +6,20 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import io.activated.pipeline.Constants;
 import io.activated.pipeline.Context;
 import java.util.List;
+import java.util.UUID;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import reactor.core.publisher.Mono;
 
 public class SessionKeyStrategyTest {
 
   private SessionKeyStrategy unit;
 
-  private final String sessionId = "test-session-id";
+  private final String sessionIdValid = UUID.randomUUID().toString();
 
   @BeforeEach
   public void setUp() {
@@ -35,16 +40,40 @@ public class SessionKeyStrategyTest {
   public void get() {
 
     var context = new Context();
-    context.getHeaders().put(Constants.SESSION_ID_CONTEXT_KEY, List.of(sessionId));
+    context.getHeaders().put(Constants.SESSION_ID_CONTEXT_KEY, List.of(sessionIdValid));
 
     var got =
         Mono.from(unit.apply(context))
-            .contextWrite(ctx -> ctx.put(Constants.SESSION_ID_CONTEXT_KEY, sessionId))
+            .contextWrite(ctx -> ctx.put(Constants.SESSION_ID_CONTEXT_KEY, sessionIdValid))
             .block();
 
     var reference = new Key();
 
-    reference.setValue(sessionId);
+    reference.setValue(sessionIdValid);
     assertThat(got).isEqualTo(reference);
+  }
+
+  @ParameterizedTest
+  @MethodSource("get_invalidData")
+  public void get_invalid(String input) {
+
+    var context = new Context();
+    context.getHeaders().put(Constants.SESSION_ID_CONTEXT_KEY, List.of(input));
+
+    assertThatThrownBy(
+            () ->
+                Mono.from(unit.apply(context))
+                    .contextWrite(ctx -> ctx.put(Constants.SESSION_ID_CONTEXT_KEY, sessionIdValid))
+                    .block())
+        .isInstanceOf(IllegalStateException.class)
+        .hasMessage("invalid pipeline-session-id");
+  }
+
+  public static Stream<Arguments> get_invalidData() {
+
+    return Stream.of(
+        Arguments.of("test-id"),
+        Arguments.of("00000000-0000-0000-0000-000000000000"),
+        Arguments.of("00000000-0000-0000-0000-000000000001"));
   }
 }
