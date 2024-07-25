128c674d774ceb3315738e388ab6e19c8c0d642c
Ben Tomasini <btomasini@activated.io>
2024-07-25 14:08:42 -0700

    refactored dependencies and tests

---
+++ core/src/main/java/io/activated/pipeline/ApplicationRuntimeException.java
@@ -0,0 +1,19 @@
+package io.activated.pipeline;
+
+public class ApplicationRuntimeException extends RuntimeException {
+
+    public ApplicationRuntimeException() {
+    }
+
+    public ApplicationRuntimeException(String message) {
+        super(message);
+    }
+
+    public ApplicationRuntimeException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ApplicationRuntimeException(Throwable cause) {
+        super(cause);
+    }
+}
